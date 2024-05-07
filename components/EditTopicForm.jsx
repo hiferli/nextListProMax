4adac369d5fe11e9938fa32806959b7735f00ec0
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-05-07 20:19:32 +0530

    Creating Edit and Add Forms

---
+++ components/EditTopicForm.jsx
@@ -0,0 +1,20 @@
+export default function EditTopicForm () {
+    return (
+        <form className="flex flex-col gap-3">
+            <input
+                className="border border-slate-500 px-8 py-2"
+                type="text"
+                placeholder="Topic Title"
+            />
+
+            <input
+                className="border border-slate-500 px-8 py-2"
+                type="text"
+                placeholder="Topic Description"
+            />
+
+            <button className="bg-red-500 font-bold text-white px-6 py-3 w-fit">Update Topic</button>
+        </form>
+    )
+}
+
